16835033fd9ec474de6e28324796f6b9b6dd48f0
Vivek Singh <vivekkrvks@gmail.com>
2023-05-28 20:27:18 +0530

    model deleted

--- routes/api/v1/public/enquiry.js
+++ routes/api/v1/public/enquiry.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require("mongoose");
 
 // Load Enquiry Model
 const Enquiry = require("../../../../Models/Public/Enquiry");
